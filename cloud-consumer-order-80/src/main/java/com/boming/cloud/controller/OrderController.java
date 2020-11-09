a314accd76c264344b3471db3d4218d56120814e
zhangboming <pandaball95@gmail.com>
2020-11-09 20:12:49 +0800

    创建公共工程

--- cloud-consumer-order-80/src/main/java/com/boming/cloud/controller/OrderController.java
+++ cloud-consumer-order-80/src/main/java/com/boming/cloud/controller/OrderController.java
@@ -5,21 +5,26 @@ import com.boming.cloud.entities.PaymentEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController("/consumer/order")
 public class OrderController {
 
-    private static final String PAYMENT_URL = "http://localhost:8001/provider/";
+    private static final String PAYMENT_URL = "http://localhost:8001/provider/payment";
 
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/add")
     public CommonResult add(PaymentEntity paymentEntity){
-        //this.restTemplate.
-        return null;
+        return restTemplate.postForObject(PAYMENT_URL + "/add", paymentEntity, CommonResult.class);
+    }
+
+    @GetMapping("/get/{id}")
+    public CommonResult<PaymentEntity> getPayment(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAYMENT_URL + "/get/" + id, CommonResult.class);
     }
 
 }
